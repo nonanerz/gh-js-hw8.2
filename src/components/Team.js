0251d45089126dff10a3d8e5622aef6ffe34a721
liudmyla_schastlyvets <lyudmila.zoria@n3wnormal.com>
2017-12-06 17:06:56 +0200

    Added base for Services and Portfolio Sections

---
+++ src/components/Team.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Team extends  Component {
+    render() {
+        return (
+            <div>
+                <p>Team</p>
+            </div>
+        )
+    }
+}
+
+export default Team
