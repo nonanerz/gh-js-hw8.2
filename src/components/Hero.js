7011a34e66152246cd259b9ff5118d3720c508e9
liudmyla_schastlyvets <lyudmila.zoria@n3wnormal.com>
2017-12-06 19:57:39 +0200

    Added base for hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,9 +3,27 @@ import React, { Component } from 'react'
 class Hero extends  Component {
     render() {
         return (
-            <div>
-                <p>Hero</p>
-            </div>
+            <section className="hero section">
+                <div className="container">
+                    <div className="hero-content">
+                        <div className="hero-information">
+                            <h1 className="main-title">ONE PAGE </h1>
+                            <span>IS ALL THAT YOU NEED</span>
+                            <p className="description">
+                                Suspendisse mi nisl, adipiscing sit amet tellus sed, auctor aliquet felis. Aenean neque elit,
+                                blandit quis tempor eu, feugiat rhoncus lectus. Quisque sit amet pretium risus.
+                            </p>
+                            <div className="buttons">
+                                <a href="#team" className="button">Our Team</a>
+                                <a href="#contact" className="button">Contact us</a>
+                            </div>
+                        </div>
+                        <div className="hero-image">
+                            <img src={require('../images/mac-mock-up.png')} alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </section>
         )
     }
 }
